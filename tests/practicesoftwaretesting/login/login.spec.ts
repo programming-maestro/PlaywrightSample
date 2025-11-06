5b07cb71695ea6ba6e31e78d5e2954280026845a
programming-maestro <csm.exclusive@gmail.com>
2025-11-07 02:08:30 +0530

    implemented Page object and environemnt config init

--- tests/practicesoftwaretesting/login/login.spec.ts
+++ tests/practicesoftwaretesting/login/login.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../../pages/loginPage';
 
-test('login', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/');
+test('login without page object', async ({ page }) => {
+  await page.goto(process.env.URL+""); // in other to use env file. ensure the baseURL is not present in both .env file and playwright config file.
+
   await page.locator('[data-test="nav-sign-in"]').click();
   await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
   await page.locator('[data-test="password"]').fill('elcome01');
@@ -14,3 +16,16 @@ test('login', async ({ page }) => {
   await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
   await expect(page.locator('[data-test="nav-messages"]')).toBeVisible();
 });
+
+test('login with page object', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto()
+
+  await loginPage.login('customer@practicesoftwaretesting.com','welcome01')
+
+  await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe');
+  await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
+  await expect(page.locator('[data-test="nav-messages"]')).toBeVisible();
+});
+
+
